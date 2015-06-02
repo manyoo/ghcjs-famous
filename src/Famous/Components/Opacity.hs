80b19a98866bb5d37e02f00f20dc66738b27a68f
Eric Wong <ericsyw@gmail.com>
2015-06-02 21:32:02 +0800

    Add setter function for opacity with transition

--- src/Famous/Components/Opacity.hs
+++ src/Famous/Components/Opacity.hs
@@ -3,8 +3,10 @@ module Famous.Components.Opacity where
 
 import GHCJS.Foreign
 import GHCJS.Types
+import GHCJS.Marshal
 
 import Famous.Core.Node
+import Famous.Transitions.Curves
 
 data Opacity_
 
@@ -23,6 +25,19 @@ foreign import javascript safe "($1).get()"
   fms_getOpacity :: Opacity -> Double
 
 getOpacity = fms_getOpacity
+
+-- | set the opacity value with transition
+foreign import javascript unsafe "($3).set($1, $2)"
+  fms_setCurve :: Double -> JSRef Curve -> Opacity -> IO ()
+foreign import javascript unsafe "($4).set($1, $2, $3)"
+  fms_setCurveCb :: Double -> JSRef Curve -> JSFun (IO ()) -> Opacity -> IO ()
+
+set :: Double -> Curve -> Maybe (IO ()) -> Opacity -> IO ()
+set ov c Nothing op = do jsc <- toJSRef c
+                         fms_setCurve ov jsc op
+set ov c (Just f) op = do jsc <- toJSRef c
+                          cb <- syncCallback AlwaysRetain True f
+                          fms_setCurveCb ov jsc cb op
 
 -- | Stops Opacity transition
 foreign import javascript unsafe "($1).halt()"
